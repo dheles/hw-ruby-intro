a95c262b2506b169c2b4d69109833c04e174df12
dheles <drew@codhicitta.com>
2016-06-20 08:45:59 -0400

    completes HW1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,67 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0
+  arr.each { |item| sum += item }
+  return sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  while arr.length < 2
+    arr.push(0)
+  end
+  sum = 0
+  working = arr.sort.reverse
+  sum += working[0]
+  sum += working[1]
+  return sum
 end
 
+# sum_to_n?([-1,-2,3,4,6,-8], 12)
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.combination(2).each do |subarr|
+    if subarr[0] + subarr[1] == n
+      return true
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s.downcase.match(/\A[[[:alpha:]]&&[^aeiou]]/)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.match(/\A[01]+\Z/)
+    return s.to_i(2) % 4 == 0
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    if isbn.empty?
+      raise ArgumentError.new("Must provide a non-empty ISBN")
+    end
+    @isbn = isbn
+    if price <= 0
+      raise ArgumentError.new("Must provide a price more than nothing")
+    end
+    @price = price.to_f
+  end
+
+  def price_as_string
+    return "$#{'%.2f' % @price}"
+  end
 end
